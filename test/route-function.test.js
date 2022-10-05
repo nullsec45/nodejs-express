9a55b705fc665f93bf78d51a9c2b118c945dfc5c
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-10-05 18:36:39 +0700

    route function

---
+++ test/route-function.test.js
@@ -0,0 +1,23 @@
+import request from "supertest";
+import express from "express";
+
+const app=express();
+app.route("/products")
+   .get((req,res) =>{
+        res.send("Get Products");
+   }).post((req,res) =>{
+        res.send("Create Products");
+   }).put((req,res) =>{
+        res.send("Update Products");
+   });
+
+test("Route Function", async () => {
+    let response=await request(app).get("/products");
+    expect(response.text).toBe("Get Products");
+
+    response=await request(app).post("/products");
+    expect(response.text).toBe("Create Products");
+
+    response=await request(app).put("/products");
+    expect(response.text).toBe("Update Products");
+});
